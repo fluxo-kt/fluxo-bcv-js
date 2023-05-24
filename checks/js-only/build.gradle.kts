7d041ec222c237f58f6d9531e804fbb3dddc307f
Artyom Shendrik <artyom.shendrik@gmail.com>
2023-05-24 22:18:54 +0200

    test: downgrade Binary Compatibility Validator to 0.8.0 for `check-js-only` example
    Signed-off-by: Artyom Shendrik <artyom.shendrik@gmail.com>

--- checks/js-only/build.gradle.kts
+++ checks/js-only/build.gradle.kts
@@ -1,7 +1,7 @@
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     kotlin("js") version libs.versions.kotlinMin
-    id("org.jetbrains.kotlinx.binary-compatibility-validator") version libs.versions.bcv
+    id("org.jetbrains.kotlinx.binary-compatibility-validator") version libs.versions.bcvMin
     id("io.github.fluxo-kt.binary-compatibility-validator-js")
 }
 
